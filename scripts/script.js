14538f07155fa86438f99a2942822f0bb0c16422
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2017-01-01 17:18:11 -0600

    Comment cleanup.
    Cleaned up some old useless comments and clarified on a few things.

--- scripts/script.js
+++ scripts/script.js
@@ -34,33 +34,39 @@ $(".color-circle").click(function(){
 	$(".color-circle").css("border-color", "transparent");
 	// Highlight this circle.
 	$(this).css("border-color", "#d3d3e2");
-	// If this color is "fade", set ourColor to fade and call the hover function without using a parameter.
+	// If this color is "fade", set ourColor to fade.
 	if($(this).data("color") === "fade") {
 		colorSetting = "fade";
-	// Else if color is "random", set ourColor to random and call the hover function without using a parameter.
+	// Else if color is "random", set ourColor to random and.
 	} else if($(this).data("color") === "random") {
 		colorSetting = "random";
-	// Otherwise set ourColor to the current circle's respective color and send it as a parameter to the hover function.
+	// Otherwise set ourColor to the current circle's respective color.
 	} else {
 	ourColor = $(this).data("color");
 	}
 });
 
-// Hover Effect NOTE, WHEN AN EVENT IS CALLED(THROUGH FUNCTION OR NOT) IT "ATTACHES" TO IT'S RELATED ELEMENT. THIS IS WHY WE CALL IT EVERYTIME WE RESET THE GRID DUE TO HAVING NEW DIVS.
+// Hover Effect. Note: When events are called(through functions, window loads, etc) they are ATTACHED to their target elements. Initially I had issues where I was attaching it multiple times and
+// I later realized by using alerts that it was firing off more than once. We also wrap this in a function so that after deleted old DIVs and creating new DIVs we need to reattach this event to the new DIVs.
 function hoverEffect() {
 	$(".grid-square").mouseenter(function(){
-		// Check if the ourColor is set to random, if it is we will change the color parameter to a random color. This triggers on each hover.
+		// Check if the ourColor is set to random, if it is we will change ourColor to a random color.
 		if(colorSetting === "random") {
 			ourColor = '#'+Math.random().toString(16).substr(2,6);
+		// else if ourColor is set to fade, we will begin changing opacity instead.
 		} else if(colorSetting === "fade") {
+			// Background has to be set to black so that we get a fade from white to black when increasing opacity.
+			ourColor = "black";
+			// Checks if opacity is default(1) and if so, sets it to 0(fully transparent).
 			if($(this).css("opacity") === "1") {
 				$(this).css("opacity", "0");
 			}
+		// As long as the div we're on has a transparency lower than default(1) we will increase it; increasing as high as .90.
 			if($(this).css("opacity") < 0.9) {
 				$(this).css("opacity", "+=0.1");
-				ourColor = "black";
 			}
 		} else {
+			// By default we need opacity to be 1(no transparency)
 			$(this).css("opacity","1");
 		}
 		$(this).css("background-color", ourColor);
