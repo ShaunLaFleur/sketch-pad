bc15c2507ef3f4914b37767ddd58ff6fa983d5d0
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2017-01-02 23:19:34 -0600

    Added new functionality.
    Added additional features and reduced the amount of times I called
    functions by placing them more effeciently.

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,54 @@ var gridSize = 20;
 var squareSize = 800/gridSize;
 var ourColor = "blue";
 var colorSetting = "";
-var isBorder = true;
+var isBorder = false;
 
 // Loadup the grid with default values when window loads.
 $(document).ready(function(){
-	$("#bluecircle").css("border-color", "black");
 	gridStart(); // Populate grid with default settings.
-	hoverEffect(); // Call the hover function to get it attached.
-	setBorder(); // Call the setBorder function to get it attached.
+	swabStart(); // Color Swab generator.
 });
+
+// Generate Sketch Pad
+function gridStart() {
+	// Create rows
+	for(i=0; i<gridSize*gridSize; i++) {
+  	$("#container").append("<div class='grid-square'></div>")
+	}
+
+	// Set Proper Size
+	$(".grid-square").css("height", squareSize-2); // -2 to make up for the 1px border on each side.
+	$(".grid-square").css("width", squareSize-2); // Same as above.
+
+	// Set border/grid properly.
+	if(!isBorder) {
+		$(".grid-square").css("border", "1px solid white");
+	} else {
+		$(".grid-square").css("border", "1px solid #885a07");
+	}
+	// Call the hover function to get it attached.
+	hoverEffect();  
+}
+
+// Generate Color Swab
+function swabStart() {
+	for(i=0; i<225; i++) {
+		$("#contain-colors").append("<div class='grid-color' data-color=''></div>");
+		var randColor = '#'+Math.random().toString(16).substr(2,6);
+		$(".grid-color").last().css("background-color", randColor);
+		$(".grid-color").last().data("color", randColor);
+	}
+	// Call the color swab to get it attached.
+	colorSwab();
+
+	// Call the swabhover event to get it attached.
+	swabHover();
+}
 
 // Reset Button
 $("#clear-grid").click(function(){	
   	$(".grid-square").remove();
   	gridStart();
-  	hoverEffect();
 });
 
 // Resize Button
@@ -25,28 +58,69 @@ $("#resize").click(function(){
  	gridSize = parseInt(prompt("Choose a pixel density. e.g 100 = 100x100 pixels dense.", "100"));
  	squareSize = 800/gridSize;
  	gridStart();
- 	hoverEffect();
 });
 
-// Color Swab
-$(".color-circle").click(function(){
-	// Reset Color Setting
-	colorSetting = "";
-	// Unhighlight any highlighted circles.
-	$(".color-circle").css("border-color", "transparent");
-	// Highlight this circle.
-	$(this).css("border-color", "black");
-	// If this color is "fade", set ourColor to fade.
-	if($(this).data("color") === "fade") {
-		colorSetting = "fade";
-	// Else if color is "random", set ourColor to random and.
-	} else if($(this).data("color") === "random") {
-		colorSetting = "random";
-	// Otherwise set ourColor to the current circle's respective color.
+// Grid Toggle Button
+$("#borderbutton").click(function(){
+	// If isBorder is true(on), we turn it off.
+	if(isBorder) {
+		// Run through each grid square and change it's border from black to the color of it's current background. This is how we toggle off the grid.
+		$(".grid-square").each(function(){
+			var resetColor = $(this).css("background-color");
+			$(this).css("border", "1px solid "+resetColor);
+			isBorder = false;
+			$("button#borderbutton").html("Grid: Off");
+		});
+	// Otherwise if border is false(off), we turn it on and put a border on all grid-squares.
 	} else {
-	ourColor = $(this).data("color");
+		$(".grid-square").css("border", "1px solid #885a07");
+		isBorder = true;
+		$("button#borderbutton").html("Grid: On");
 	}
 });
+
+// Regenerate Button
+$("#regencolors").click(function(){
+	$(".grid-color").remove(); // REmove all grid colors.
+	swabStart(); // Repopulate the grid with colors.
+});
+
+// Color Swab Click
+function colorSwab() {
+	$(".fade-rain, .grid-color").click(function(){
+		// Reset Color Setting
+		colorSetting = "";
+		// Unhighlight any highlighted circles.
+		$(".fade-rain").css("border-color", "white");
+		// Highlight this circle.
+		$(this).css("border-color", "black");
+		// If this color is "fade", set ourColor to fade.
+		if($(this).data("color") === "fade") {
+			colorSetting = "fade";
+			$("#selected").css("background-color", "transparent");
+		// Else if color is "random", set ourColor to random and.
+		} else if($(this).data("color") === "random") {
+			colorSetting = "random";
+			$("#selected").css("background-color", "transparent");
+		// Otherwise set ourColor to the current circle's respective color.
+		} else {
+			ourColor = $(this).data("color");
+			$("#selected").css("background-color", ourColor);
+		}		
+	});
+}
+
+// Color Swab Hover
+function swabHover() {
+	$(".grid-color").mouseenter(function(){
+		var preview = $(this).css("background-color");
+		$("#highlighted").css("background-color", preview);
+	});
+
+	$(".grid-color").mouseleave(function(){
+		$("#highlighted").css("background-color", "");
+	});
+}
 
 // Hover Effect. Note: When events are called(through functions, window loads, etc) they are ATTACHED to their target elements. Initially I had issues where I was attaching it multiple times and
 // I later realized by using alerts that it was firing off more than once. We also wrap this in a function so that after deleted old DIVs and creating new DIVs we need to reattach this event to the new DIVs.
@@ -78,41 +152,4 @@ function hoverEffect() {
 			$(this).css("border", "1px solid "+ourColor);
 		}
 	});
-}
-
-function setBorder() {
-	$("#borderbutton").click(function(){
-		// If isBorder is true(on), we turn it off.
-		if(isBorder) {
-			// Run through each grid square and change it's border from black to the color of it's current background. This is how we toggle off the grid.
-			$(".grid-square").each(function(){
-				var resetColor = $(this).css("background-color");
-				$(this).css("border", "1px solid "+resetColor);
-				isBorder = false;
-				$("button#borderbutton").html("Grid: Off");
-			});
-		// Otherwise if border is false(off), we turn it on and put a border on all grid-squares.
-		} else {
-			$(".grid-square").css("border", "1px solid #885a07");
-			isBorder = true;
-			$("button#borderbutton").html("Grid: On");
-		}
-	});
-}
-
-
-function gridStart() {
-	// Create rows
-	for(i=0; i<gridSize*gridSize; i++) {
-  	$("#container").append("<div class='grid-square'></div>")
-	}
-
-	// Set Proper Size
-	$(".grid-square").css("height", squareSize-2);
-	$(".grid-square").css("width", squareSize-2);
-
-	// Set border/grid properly.
-	if(!isBorder) {
-		$(".grid-square").css("border", "1px solid white");
-	}
 }
