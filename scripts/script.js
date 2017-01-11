a51e37f8d21611631c452d32e628c82aa3e75ad2
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2017-01-11 01:39:48 -0600

    Small changes.

--- scripts/script.js
+++ scripts/script.js
@@ -23,7 +23,7 @@ function gridStart() {
 
 	// Set border/grid properly.
 	if(isBorder) {
-		$(".grid-square").css("border", "1px solid #4286f4");
+		$(".grid-square").css("border-color", "#4286f4");
 	}
 	
 // Attach mouseenter effect
@@ -59,16 +59,16 @@ function swabStart() {
 	for(i=0; i<225; i++) {
 		$("#contain-colors").append("<div class='grid-color' data-color=''></div>");
 		var randColor = '#'+Math.random().toString(16).substr(2,6);
-		$(".grid-color").last().css("background-color", randColor);
-		$(".grid-color").last().data("color", randColor);
+		$(".grid-color").eq(i).css("background-color", randColor);
+		$(".grid-color").eq(i).data("color", randColor);
 	}
 
 	// Color Swab Click
 	$(".fade-rain, .grid-color").click(function(){
 		// Unhighlight.
-		$(".fade-rain").css("border-color", "white");
+		$(".fade-rain").css("border-color", "transparent");
 		// Highlight this option.
-		$(this).css("border-color", "black");
+		$(this).css("border-color", "#4286f4");
 		// If this color is "fade", set ourColor to fade.
 		if($(this).data("color") === "fade") {
 			colorSetting = "fade";
@@ -117,12 +117,12 @@ $("#borderbutton").click(function(){
 	// If isBorder is true(on), we turn it off.
 	if(isBorder) {
 			//Toggle the grid off by changing it's border to transparent.
-			$(".grid-square").css("border", "1px solid transparent");
+			$(".grid-square").css("border-color", "transparent");
 			isBorder = false;
 			$("button#borderbutton").html("Grid: Off");
 	// Otherwise if border is false(off), we turn it on and put a border on all grid-squares.
 	} else {
-		$(".grid-square").css("border", "1px solid #4286f4");
+		$(".grid-square").css("border-color", "#4286f4");
 		isBorder = true;
 		$("button#borderbutton").html("Grid: On");
 	}
